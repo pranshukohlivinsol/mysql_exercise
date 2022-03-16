26463d9205d01c3f5c470d206415b26d0208c32c
pranshukohlivinsol <101559749+pranshukohlivinsol@users.noreply.github.com>
2022-03-16 22:04:01 +0530

    Update queries.sql

--- exercise_3/queries.sql
+++ exercise_3/queries.sql
@@ -2,13 +2,14 @@
 CREATE:
 INSERT INTO users (name, type) values ("user_1", "admin");
 INSERT INTO users (name, type) values ("user_2", "normal");
+INSERT INTO users (name, type) values ("user_3", "normal");
 
 INSERT INTO categories (name) values ("tech");
 INSERT INTO categories (name) values ("stocks");
 INSERT INTO categories (name) values ("gaming");
 
 INSERT INTO articles (title, category_id, user_id, created_on) values ("How to reduce ping??", 3, 2, NOW());
-INSERT INTO articles_body (article_id,body) values(3,"<P><H3>There are many ways to reduce ping<H3><BR><span>Article 3</span></P>");
+INSERT INTO articles_body (article_id,body) values(3, "<P><H3>There are many ways to reduce ping<H3><BR><span>Article 3</span></P>");
 
 2.
 SET @U3="user_3";
