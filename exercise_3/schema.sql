74ebf4ae9a5484930cea78d5e0d24187bb443d60
pranshukohlivinsol <101559749+pranshukohlivinsol@users.noreply.github.com>
2022-03-16 20:07:02 +0530

    Update schema.sql

--- exercise_3/schema.sql
+++ exercise_3/schema.sql
@@ -1,2 +1,5 @@
-CREATE TABLE IF NOT EXISTS users (id int AUTO_INCREMENT PRIMARY KEY, name varchar(40) NOT NULL UNIQUE, type ENUM('admin','normal') NOT NULL);
-CREATE TABLE IF NOT EXISTS categories (id int AUTO_INCREMENT PRIMARY KEY, name VARCHAR(40) NOT NULL UNIQUE);
+CREATE TABLE IF NOT EXISTS users (id int AUTO_INCREMENT PRIMARY KEY, name varchar(40) NOT NULL UNIQUE, type enum('admin','normal') NOT NULL);
+CREATE TABLE IF NOT EXISTS categories (id int AUTO_INCREMENT PRIMARY KEY, name varchar(40) NOT NULL UNIQUE);
+CREATE TABLE IF NOT EXISTS articles (id int AUTO_INCREMENT PRIMARY KEY, title varchar(255) NOT NULL, category_id int NOT NULL, user_id int NOT NULL, created_on DATETIME NOT NULL);
+CREATE TABLE IF NOT EXISTS articles_body (article_id int NOT NULL, body text);
+CREATE TABLE IF NOT EXISTS comments (id int AUTO_INCREMENT PRIMARY KEY, article_id int NOT NULL, user_id int NOT NULL, created_on DATETIME NOT NULL);
