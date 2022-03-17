7ed4e5af1721145cdaec84677567e390d0de1960
pranshukohlivinsol <101559749+pranshukohlivinsol@users.noreply.github.com>
2022-03-17 13:15:01 +0530

    Update schema.sql

--- transaction/schema.sql
+++ transaction/schema.sql
@@ -1,4 +1,4 @@
-create database bank;
-use bank;
+CREATE DATABASE bank;
+USE bank;
 CREATE TABLE IF NOT EXISTS users (id int AUTO_INCREMENT PRIMARY KEY, name varchar(40) NOT NULL, email varchar(40) NOT NULL UNIQUE, account_no BIGINT NOT NULL UNIQUE);
 CREATE TABLE IF NOT EXISTS accounts (id int AUTO_INCREMENT PRIMARY KEY, account_no NOT NULL UNIQUE, balance int NOT NULL);
