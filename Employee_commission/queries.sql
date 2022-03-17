50628ea5401b520f61cb732513e9f24cbf1ebe7d
pranshukohlivinsol <101559749+pranshukohlivinsol@users.noreply.github.com>
2022-03-17 15:17:36 +0530

    Update queries.sql

--- Employee_commission/queries.sql
+++ Employee_commission/queries.sql
@@ -1,1 +1,12 @@
+1.
+SELECT e.name, SUM(commission_amount) 
+  FROM commissions AS c JOIN employees AS e ON c.employee_id=e.id 
+  GROUP BY employee_id LIMIT 1;
+SELECT name, max(comm) FROM (
+  SELECT e.name AS name, sum(commission_amount) AS comm  
+    FROM commissions AS c JOIN employees AS e ON c.employee_id=e.id 
+    GROUP BY employee_id
+  ) AS A;
+
+2.
 
