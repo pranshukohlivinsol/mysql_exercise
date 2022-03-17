64e8d2d70b29688e40556493a9388fd5dbbf6189
pranshukohlivinsol <101559749+pranshukohlivinsol@users.noreply.github.com>
2022-03-17 15:28:27 +0530

    Update queries.sql

--- Employee_commission/queries.sql
+++ Employee_commission/queries.sql
@@ -2,11 +2,26 @@
 SELECT e.name, SUM(commission_amount) 
   FROM commissions AS c JOIN employees AS e ON c.employee_id=e.id 
   GROUP BY employee_id LIMIT 1;
-SELECT name, max(comm) FROM (
-  SELECT e.name AS name, sum(commission_amount) AS comm  
-    FROM commissions AS c JOIN employees AS e ON c.employee_id=e.id 
-    GROUP BY employee_id
+  
+SELECT name, max(comm) 
+  FROM (
+    SELECT e.name AS name, sum(commission_amount) AS comm  
+      FROM commissions AS c JOIN employees AS e ON c.employee_id=e.id 
+      GROUP BY employee_id
   ) AS A;
 
-2.
 
+2.
+SELECT salary 
+  FROM employees 
+  ORDER BY salary DESC 
+  LIMIT 3,1;
+
+
+3.
+SELECT d.name, max(comm) 
+  FROM (
+    SELECT e.name AS name, SUM(commission_amount) AS comm, e.department_id AS dept  
+    FROM commissions AS c JOIN employees AS e ON c.employee_id=e.id 
+    GROUP BY employee_id
+  ) AS a JOIN departments AS d ON a.dept=d.id;
