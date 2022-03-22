50f97056faa049d47a99cce9aa3c710180730148
pranshukohlivinsol <101559749+pranshukohlivinsol@users.noreply.github.com>
2022-03-22 12:54:18 +0530

    Update queries.sql

--- Employee_commission/queries.sql
+++ Employee_commission/queries.sql
@@ -26,8 +26,8 @@ SELECT sum(commission_amount) AS comm, e.department_id AS dept_id, d.name as dep
   FROM commissions AS c JOIN employees AS e JOIN departments as d 
   ON c.employee_id=e.id  AND e.department_id=d.id   
   GROUP BY d.id 
-  ORDER BY comm 
-  DESC LIMIT 1;
+  ORDER BY comm DESC
+  LIMIT 1;
   
 4.
 SELECT e.name, sum(c.commission_amount) as total_commission 
